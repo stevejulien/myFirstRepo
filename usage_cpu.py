91fc6279d53eb34246677adce751a5f7811cfc98
stevejulien <julien.bessakanga@gmail.com>
2022-01-25 11:08:49 +0100

    add usage_cpu program

---
+++ usage_cpu.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import os
+import psutil
+
+# Getting loadover15 minutes
+load1, load5, load15 = psutil.getloadavg()
+
+cpu_usage = (load15/os.cpu_count()) * 100
+
+print("The CPU usage is : ", cpu_usage)
