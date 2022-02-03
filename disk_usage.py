7d55ca12c86c22f4166a8718a24aed03c522dbde
julien <julien.bessakanga@gmail.com>
2022-02-03 14:50:05 +0000

    add memory check pourcentage to disk_usage.py

--- disk_usage.py
+++ disk_usage.py
@@ -11,8 +11,8 @@ def check_disk_usage(disk, min_absolute, min_percent):
 	   if percent_free < min_percent or gigabytes_free < min_absolute:
 	   	return False
 	   return True
-  # Check for at least 2 GB and 10% free
-if not check_disk_usage("/", 2, 10):
+  # Check for at least 20 GB and 10% free
+if not check_disk_usage("/", 20, 10):
  	print("ERROR: Not enough disk space")
  	sys.exit(1)
 
